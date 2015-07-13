60f6a1367383a7d55aef7cc228b32ecfcc9ef040
Autocrit <tom@Toms-Mac-Pro.home>
2015-07-13 17:05:38 +0100

    Eval with find

--- eval-test.sh
+++ eval-test.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-INCLUDE_HIDDEN=1
+INCLUDE_HIDDEN=0
 
-source1="$1"
+source="$1"
 copy1="$2"
 
-include_hidden_opt="--exclude=\".*\""
+log_folder="$HOME""/Library/Logs/kitchen-sync"
+log_file="$log_folder""/log.txt"
+
+mkdir -p "$log_folder"
+
+#include_hidden_opt="--exclude=\".*\""
+include_hidden_opt="-not -name \".*\""
 
 if [ "$INCLUDE_HIDDEN" == 1 ] ; then
 	include_hidden_opt=""
 fi
 
 #echo "'$include_hidden_opt'"
-eval rSync -rtnv " $include_hidden_opt " "$source1" "$copy1"
+#eval rSync -rtnv " $include_hidden_opt " "$source" "$copy1"
+
+cd "$source"
+
+echo "checksums on ""$source"
+#eval find -s * -type f " $include_hidden_opt " "-exec md5 -r \"{}\" \;"
+eval "find -s * -type f ""$include_hidden_opt" " -exec md5 -r \"{}\" \; >> ""$log_file"
