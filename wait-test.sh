d96b0c2c9c33bfe6454569fbe95db79ac791c518
Autocrit <tom@Toms-Mac-Pro.home>
2015-07-13 09:35:17 +0100

    More

--- wait-test.sh
+++ wait-test.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-echo "Starting copy 1"
+source="source"
+copies[0]="copy1"
+copies[1]="copy2"
+copies[2]="copy3"
+
+echo "Starting copy 1: ""$source"" -> ""${copies[0]}"
 sleep $(($RANDOM % 10 + 10))
-echo "Finished copy 1"
+echo "Finished copy 1: ""$source"" -> ""${copies[0]}"
 
 {
-	echo "Starting verify 1"
+	echo "Starting checksums: ""$source"
 	sleep $(($RANDOM % 10 + 10))
-	echo "Finished verify 1"
+	echo "Finished checksums: ""$source"
 } &
 
-for (( i = 2 ; i < 6 ; i++ )); do
+source="${copies[0]}"
+
+for (( i = 1 ; i < ${#copies[@]} ; i++ )); do
 	{
-		echo "Starting copy "$i
+		echo "Starting copy "$(($i+1))": ""$source"" -> ""${copies[i]}"
 		sleep $(($RANDOM % 10 + 10))
-		echo "Finished copy "$i
+		echo "Finished copy "$(($i+1))": ""$source"" -> ""${copies[i]}"
 		
-		echo "Starting verify "$i
+		echo "Starting checksums: ""${copies[i]}"
 		sleep $(($RANDOM % 10 + 10))
-		echo "Finished verify "$i
+		echo "Finished checksums: ""${copies[i]}"
 	} &
 done
 
