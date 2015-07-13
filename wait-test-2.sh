13b30459e9a72625ba4c0ca6ad24295b31af36bf
Autocrit <tom@Toms-Mac-Pro.home>
2015-07-13 13:20:13 +0100

    Not working yet

--- wait-test-2.sh
+++ wait-test-2.sh
@@ -5,42 +5,28 @@ copies[0]="copy1"
 copies[1]="copy2"
 copies[2]="copy3"
 
-echo "Starting copy 1: ""$source"" -> ""${copies[0]}"
-sleep $(($RANDOM % 10 + 10))
-echo "Finished copy 1: ""$source"" -> ""${copies[0]}"
-
-{
-	echo "Starting checksums: ""$source"
-	sleep $(($RANDOM % 10 + 10))
-	echo "Finished checksums: ""$source"
-} &
-
-source="${copies[0]}"
-
 for (( i = 0 ; i < ${#copies[@]} ; i++ )); do
+	{
+		echo "Starting copy "$(($i+1))": ""$source"" -> ""${copies[i]}"
+		sleep $(($RANDOM % 10 + 10))
+		echo "Finished copy "$(($i+1))": ""$source"" -> ""${copies[i]}"
 
-	if [ $i -eq 0 ]; then
-	{
-		echo "Starting copy "$(($i+1))": ""$source"" -> ""${copies[i]}"
-		sleep $(($RANDOM % 10 + 10))
-		echo "Finished copy "$(($i+1))": ""$source"" -> ""${copies[i]}"
-	}
-	fi
-	
-	{
-		echo "Starting copy "$(($i+1))": ""$source"" -> ""${copies[i]}"
-		sleep $(($RANDOM % 10 + 10))
-		echo "Finished copy "$(($i+1))": ""$source"" -> ""${copies[i]}"
-		
 		echo "Starting checksums: ""${copies[i]}"
 		sleep $(($RANDOM % 10 + 10))
 		echo "Finished checksums: ""${copies[i]}"
 	} &
 	
 	if [ $i -eq 0 ]; then
-		source="${copies[1]}"
+		wait
+
+		{
+			echo "Starting checksums: ""$source"
+			sleep $(($RANDOM % 10 + 10))
+			echo "Finished checksums: ""$source"
+		} &
+		
+		source="${copies[0]}"
 	fi
-	
 done
 
 wait
